953fa8b2a404de1f6fae829f7970849b50341a31
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2024-11-07 17:13:42 -0300

    Installs antd as component library

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+    plugins: [react(), mkcert()],
 })
