6823501cb753211839b6218ad048099f34b5e242
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2024-12-03 17:04:24 -0300

    AppDBContext auto loading config

--- server/src/Models/AppDbContext.cs
+++ server/src/Models/AppDbContext.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace fintrack.Models;
 
-public class AppDbContext(string connectionString) : DbContext(GetOptions(connectionString))
+public class AppDbContext() : DbContext(GetOptions())
 {
     public DbSet<User> Users { get; set; }
     public DbSet<UserSession> UserSessions { get; set; }
@@ -12,8 +12,10 @@ public class AppDbContext(string connectionString) : DbContext(GetOptions(connec
     public DbSet<StockHolding> StockHoldings { get; set; }
     public DbSet<HoldingGroupDailyAgg> HoldingGroupDailyAggs { get; set; }
 
-    private static DbContextOptions<AppDbContext> GetOptions(string connectionString)
+    private static DbContextOptions<AppDbContext> GetOptions()
     {
+        const string connectionString =
+            "Host=localhost;Database=postgres;Username=postgres;Password=postgres;Port=5435;";
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
         optionsBuilder.UseNpgsql(connectionString);
         return optionsBuilder.Options;
